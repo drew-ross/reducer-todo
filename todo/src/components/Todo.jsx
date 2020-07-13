0fb9c71333f37c15ca8a5885f1cf3f2ad6a6fcd5
Drew Ross <mail.drew.ross@gmail.com>
2020-07-13 15:54:33 -0700

    Add SASS styling to app.

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { DELETE_TODO, TOGGLE_TODO } from '../reducers/todoReducer';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faTrash, faUndo } from '@fortawesome/free-solid-svg-icons';
 
 const Todo = ({ todo, dispatch }) => {
 
@@ -25,9 +27,25 @@ const Todo = ({ todo, dispatch }) => {
 
     return (
         <div className={completedClassName}>
-            <div className='todo-name'>{todo.item}</div>
-            <button className='todo-toggle-btn' onClick={handleToggle}>Complete</button>
-            <button className='todo-delete-btn' onClick={handleDelete}>Delete</button>
+            <div className='todo-name'><p>{todo.item}</p></div>
+            <button className='todo-toggle-btn' onClick={handleToggle}>
+                {todo.completed ?
+                    (
+                        <>
+                            <span className='hidden'>Un-Complete</span>
+                            <FontAwesomeIcon icon={faUndo} />
+                        </>
+                    ) : (
+                        <>
+                            <span className='hidden'>Complete</span>
+                            <FontAwesomeIcon icon={faCheck} />
+                        </>
+                    )}
+            </button>
+            <button className='todo-delete-btn' onClick={handleDelete}>
+                <span className='hidden'>Delete</span>
+                <FontAwesomeIcon icon={faTrash} />
+                </button>
         </div>
     );
 };
