429f60cb70cf0ab3c9ac441a08698c04af2cf772
Drew Ross <mail.drew.ross@gmail.com>
2020-07-13 13:38:54 -0700

    Set up todoReducer outline with four action variables.

---
+++ todo/src/reducers/todoReducer.js
@@ -0,0 +1,17 @@
+const CREATE_TODO = 'CREATE_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+const COMPLETE_TODO = 'COMPLETE_TODO';
+const CLEAR_COMPLETED = 'CLEAR_COMPLETED';
+
+const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+};
+
+export const todoReducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
